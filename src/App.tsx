53c09f2e82f726b3534f1da13484f231db6de0ea
novita19931993 <148832692+novita19931993@users.noreply.github.com>
2024-10-02 00:27:14 -0300

    new: se agrega logica para autentificacion y loop de 55 segundos para la solicitud del token refresh a la api

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 // App.tsx
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import RequestOTP from './components/RequestOTP'; // Asegúrate de tener la ruta correcta
 import Profile from './components/Profile'; // También para el perfil, si ya lo tienes
 import Login from './components/Login'; // Importa el componente Login
+import TokenManager from './components/TokenManager'; // Importa el TokenManager
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Estado para controlar la autenticación
+
+  // Función para manejar la autenticación
+  const handleAuthentication = (status) => {
+    setIsAuthenticated(status); // Cambia el estado de autenticación
+  };
+
   return (
     <Router>
+      {isAuthenticated && <TokenManager />} {/* Monta TokenManager solo si estás autenticado */}
       <Routes>
         <Route path="/" element={<RequestOTP />} /> {/* Mostrar el componente de solicitar OTP */}
-        <Route path="/login" element={<Login />} /> {/* Mostrar el componente de inicio de sesión */}
+        <Route path="/login" element={<Login setAuthenticated={handleAuthentication} />} /> {/* Pasar función a Login */}
         <Route path="/profile" element={<Profile />} /> {/* Mostrar el perfil después de autenticarse */}
       </Routes>
     </Router>
