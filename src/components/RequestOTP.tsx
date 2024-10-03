362297ec8725c785dbdec56effd5de17ba884783
novita19931993 <148832692+novita19931993@users.noreply.github.com>
2024-10-02 23:57:50 -0300

    fix: correccion de comentarios

--- src/components/RequestOTP.tsx
+++ src/components/RequestOTP.tsx
@@ -11,21 +11,25 @@ function RequestOTP() {
   // get y post se manejan mejor con operacion asincronas
   const handleRequestOTP = async () => {
     // email es un string vacio, asi que es falsy, lo invertimos para que cuando sea un string vacio, de true y muestre el mensaje
+    // si email es false , muestrame un true
     if (!email) {
       setMessage("Por favor, ingresa tu correo.");
       return;
     }
     // await es para esperar la promesa de la  data este completa, llega y sigue el codigo, solo funciona con funciones async (asincronas)
     // axios.post(url, data A enviar, config); esa es la sintaxis
+    // post se utiliza para enviar datos al servidor, la otp es la respuesta, pueden ser varias, estados http , confirmaciones etc
     try {
       await axios.post(
         "https://neovc-api-1d2f751d3888.herokuapp.com/users/get-otp/",
         { email }
       );
+      // si no tomo el catch error , la solicitud a sido exitosa.
       // localstore se guarda en el navegador, a diferencia del sessionstore persiste mas
       // localstore es un objeto que permite guardar informacion de la session en el navegador incluso si se cierra
       // funciona con clave valor. userEmail es la clave y email es la variable que sera identificada por la clave
       localStorage.setItem("userEmail", email);
+
       //navigate despues de guardar el local, nos redirecciona al login
       navigate("/login");
     } catch (error) {
@@ -44,9 +48,9 @@ function RequestOTP() {
         <input
           type="email"
           placeholder="Ingresa tu correo"
-          value={email}
+          value={email} // componente controlado por este estado, email siempre se actualizara. COMPONENTE CONTROLADO
           // escuchador de eventos sencillo inline, on-change para setear el email cuando se tipea
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)} // llamando directamente al set email en vez de crear una setvariable
           className="w-full p-3 border border-gray-300 rounded-md mb-4 focus:outline-none focus:ring-2 focus:ring-indigo-500"
         />
 
